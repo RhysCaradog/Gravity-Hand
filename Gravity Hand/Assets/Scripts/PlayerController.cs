9cbd4f23eef3d0d60ae1188f0592178ba93dd1cd
RhysCaradog08 <55874291+RhysCaradog08@users.noreply.github.com>
2020-06-16 18:18:58 +0100

    Added Jump & Gravity to Player Controller

--- Gravity Hand/Assets/Scripts/PlayerController.cs
+++ Gravity Hand/Assets/Scripts/PlayerController.cs
@@ -7,14 +7,44 @@ public class PlayerController : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 10f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
 
     void Update()
     {
+        //Checks if player is grounded
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
+        //Reset players velocity when grounded
+        if(isGrounded && velocity.y <0)
+        {
+            velocity.y = -2f;
+        }
+
+        //Movement enabled on Input * speed value
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        //Calculates appropriate jump height
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        
+        //Applies gravity to the players fall speed over time
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
