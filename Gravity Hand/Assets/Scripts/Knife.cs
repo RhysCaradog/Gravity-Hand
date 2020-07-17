d4da0eb85948e9f57ef26f09cf1cf05e09f3d718
RhysCaradog08 <55874291+RhysCaradog08@users.noreply.github.com>
2020-07-17 19:18:09 +0100

    Set up Knife Stab & Throw animations

---
+++ Gravity Hand/Assets/Scripts/Knife.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knife : MonoBehaviour
+{
+    public Camera cam;
+
+    Rigidbody rb;
+
+    public float throwForce;
+
+    Animator anim;
+    
+
+    bool thrown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("Stab");
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            anim.SetTrigger("Throw");
+        }
+    }
+
+    void Stab()
+    {
+
+    }
+
+    void ThrowKnife()
+    {
+        rb.AddForce(cam.transform.forward * throwForce, ForceMode.Impulse);
+        rb.isKinematic = false;
+
+        thrown = true;
+    }
+}
