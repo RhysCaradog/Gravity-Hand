713b3241e2755ff2d847639b0211a97194e8caa9
RhysCaradog08 <55874291+RhysCaradog08@users.noreply.github.com>
2020-07-18 19:58:37 +0100

    Sorted out Knife issues

--- Gravity Hand/Assets/Scripts/Knife.cs
+++ Gravity Hand/Assets/Scripts/Knife.cs
@@ -4,70 +4,31 @@ using UnityEngine;
 
 public class Knife : MonoBehaviour
 {
-    public GameObject knife;
-
-    public Camera cam;
-
     Rigidbody rb;
 
-    public float throwForce;
+    bool thrown = false;
 
-    Animator anim;
-
-    public Transform weaponPos;
-
-    bool hasKnife;
-
-    // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponentInChildren<Rigidbody>();
-        anim = GetComponent<Animator>();
-
-        hasKnife = true;
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter(Collider other)
     {
+        EnemyHealth enemyHealth = other.GetComponent<EnemyHealth>();
 
-        if (hasKnife)
+        if (enemyHealth != null)
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                anim.SetTrigger("Stab");
-            }
-
-            if (Input.GetMouseButtonDown(1) && hasKnife)
-            {
-                anim.SetTrigger("Throw");
-            }
-        }      
-    }
-
-    public void ThrowKnife()
-    {
-        anim.enabled = false;
-        knife.transform.parent = null;
-        rb.isKinematic = false;
-        rb.AddForce(cam.transform.forward * throwForce, ForceMode.Impulse);
-
-        hasKnife = false;
-    }
-
-    void OnCollisionEnter(Collision col)
-    {
-            EnemyHealth enemyHealth = col.collider.GetComponent<EnemyHealth>();
-
-            if (enemyHealth != null)
-            {
-                enemyHealth.TakeDamage(5);
-            }
-
-            if (col.collider && hasKnife)
-            {
-                rb.isKinematic = true;
-            }
+            enemyHealth.TakeDamage(5);
         }
     }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.other && thrown)
+        {
+            rb.isKinematic = true;
+        }
+    }
+}
 
---
+++ Gravity Hand/Assets/Scripts/KnifeController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeController : MonoBehaviour
+{
+    public GameObject knife;
+
+    public Camera cam;
+
+    Rigidbody rb;
+
+    public float throwForce;
+
+    Animator anim;
+
+    public Transform weaponPos;
+
+    bool hasKnife;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = knife.GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+
+        hasKnife = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (hasKnife)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                anim.SetTrigger("Stab");
+            }
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                anim.SetTrigger("Throw");
+            }
+        }
+
+        if(!hasKnife)
+        {
+
+        }
+
+
+    }
+
+    public void ThrowKnife()
+    {
+        anim.enabled = false;
+        knife.transform.parent = null;
+        rb.isKinematic = false;
+        rb.AddForce(cam.transform.forward * throwForce, ForceMode.Impulse);
+
+        hasKnife = false;
+    }
+}
